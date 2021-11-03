6ffc522e05485e91a5882590286eb7001d834782
Rajith Jayakuru <pandukajayakuru@gmail.com>
2021-11-03 19:08:31 -0400

    Stdin Setup

--- play.js
+++ play.js
@@ -1,2 +1,19 @@
 const {connect} = require('./client')
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+
+
+const handleUserInput = function () {
+  // your code here
+};
+
+setupInput();
